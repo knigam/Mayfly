cbb188c4b549e9b4f91d22118bbd526e8a7c6d40
knigam <kushal.nigam1@gmail.com>
2014-01-23 19:58:24 -0500

    added facebook authentication

--- Mayfly/src/main/java/com/keonasoft/mayfly/MainActivity.java
+++ Mayfly/src/main/java/com/keonasoft/mayfly/MainActivity.java
@@ -1,9 +1,12 @@
 package com.keonasoft.mayfly;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,55 +14,117 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.LoginButton;
+
+public class MainActivity extends FragmentActivity {
+
+    private MainFragment mainFragment;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
 
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+            // Add the fragment on initial activity setup
+            mainFragment = new MainFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, mainFragment)
                     .commit();
+        } else {
+            // Or set the fragment from restored state info
+            mainFragment = (MainFragment) getSupportFragmentManager()
+                    .findFragmentById(android.R.id.content);
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     /**
-     * A placeholder fragment containing a simple view.
+     * Fragment for Facebook login
      */
-    public static class PlaceholderFragment extends Fragment {
+    public class MainFragment extends Fragment {
 
-        public PlaceholderFragment() {
+        private static final String TAG = "MainFragment";
+        private UiLifecycleHelper uiHelper;
+        private Session.StatusCallback callback = new Session.StatusCallback() {
+            @Override
+            public void call(Session session, SessionState state, Exception exception) {
+                onSessionStateChange(session, state, exception);
+            }
+        };
+
+        public MainFragment() {
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            uiHelper = new UiLifecycleHelper(getActivity(), callback);
+            uiHelper.onCreate(savedInstanceState);
         }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+
+            // For scenarios where the main activity is launched and user
+            // session is not null, the session state change notification
+            // may not be triggered. Trigger it if it's open/closed.
+            Session session = Session.getActiveSession();
+            if (session != null &&
+                    (session.isOpened() || session.isClosed()) ) {
+                onSessionStateChange(session, session.getState(), null);
+            }
+
+            uiHelper.onResume();
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            uiHelper.onActivityResult(requestCode, resultCode, data);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            uiHelper.onPause();
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            uiHelper.onDestroy();
+        }
+
+        @Override
+        public void onSaveInstanceState(Bundle outState) {
+            super.onSaveInstanceState(outState);
+            uiHelper.onSaveInstanceState(outState);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater,
+                                 ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.main, container, false);
+
+            LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
+            authButton.setFragment(this);
+
+            return view;
+        }
+
+        private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+           if (state.isOpened()) {
+                Log.i(TAG, "Logged in...");
+            }
+            else if (state.isClosed()) {
+                Log.i(TAG, "Logged out...");
+    }
+}
     }
 
 }
